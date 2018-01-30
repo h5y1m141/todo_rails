5ba5c833f4397097824e789cecb6d8f6c6bd5eaa
HiroshiOyamada <h5y1m141@gmail.com>
2018-01-30 17:04:09 +0900

    nginx環境で動作するように変更

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -3,28 +3,28 @@ app_path = '/myapp'
 worker_processes 3
 timeout 15
 preload_app true
-# listen "#{app_path}/tmp/unicorn.sock"
-# pid "#{app_path}/tmp/unicorn.pid"
-pid "/var/run/unicorn.pid"
-listen "/tmp/sockets/unicorn.sock"
-# before_fork do |server, worker|
-#   Signal.trap 'TERM' do
-#     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-#     Process.kill 'QUIT', Process.pid
-#   end
+listen "#{app_path}/tmp/sockets/unicorn.sock"
+pid "#{app_path}/tmp/pids/unicorn.pid"
+# pid "/var/run/unicorn.pid"
+# listen "/tmp/sockets/unicorn.sock"
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
 
-#   defined?(ActiveRecord::Base) and
-#     ActiveRecord::Base.connection.disconnect!
-# end
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
 
-# after_fork do |server, worker|
-#   Signal.trap 'TERM' do
-#     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-#   end
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
 
-#   defined?(ActiveRecord::Base) and
-#     ActiveRecord::Base.establish_connection
-# end
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
 
 # logging
 stdout_path "#{app_path}/log/unicorn.stdout.log"
